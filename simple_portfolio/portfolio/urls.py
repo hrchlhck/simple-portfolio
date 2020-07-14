2c41cb64542a99c74a22e9003295d0161059dac6
hrchlhck <vpemfh7@gmail.com>
2020-07-14 13:04:34 -0300

    Fixed path to images

--- simple_portfolio/portfolio/urls.py
+++ simple_portfolio/portfolio/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 
 from .views import Index, Projects
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', Index.as_view()),
     path('projects/', Projects.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
