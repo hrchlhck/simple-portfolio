fe213c32d99eda325b2db9a822931b7484805e04
hrchlhck <vpemfh7@gmail.com>
2020-07-29 13:22:09 -0300

    Added pinned project option to model and added this feature into projects template

--- simple_portfolio/portfolio/models.py
+++ simple_portfolio/portfolio/models.py
@@ -7,11 +7,12 @@ from os.path import join
 class Project(models.Model):
     name = models.CharField(max_length=45)
     description = models.TextField(max_length=300)
-    url = models.URLField(verbose_name="URL")
-    img = models.ImageField(verbose_name="Image", upload_to='project_images')
+    url = models.URLField(verbose_name='URL')
+    img = models.ImageField(verbose_name='Image', upload_to='project_images')
+    pinned = models.BooleanField(default=False)
     
     class Meta:
-        ordering = ['name']
+        ordering = ['-pinned', 'name']
     
     def __str__(self):
         return self.name
