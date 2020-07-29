67500c0ea921bfa3f4c93207c9707d05b96b232c
hrchlhck <vpemfh7@gmail.com>
2020-07-29 02:28:27 -0300

    Added verbose name to some fields and changed charfield to textfield

--- simple_portfolio/portfolio/models.py
+++ simple_portfolio/portfolio/models.py
@@ -5,10 +5,10 @@ from django.conf import settings
 from os.path import join
 
 class Project(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=200)
-    url = models.URLField()
-    img = models.ImageField(upload_to='project_images')
+    name = models.CharField(max_length=45)
+    description = models.TextField(max_length=300)
+    url = models.URLField(verbose_name="URL")
+    img = models.ImageField(verbose_name="Image", upload_to='project_images')
     
     class Meta:
         ordering = ['name']
